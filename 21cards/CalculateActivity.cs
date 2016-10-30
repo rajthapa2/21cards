6e6fc4df49869dfbf917a19583a01b5ccee80271
Raja Thapa <rajthapa221@gmail.com>
2016-10-30 17:59:33 +0000

    adding table for result

--- 21cards/CalculateActivity.cs
+++ 21cards/CalculateActivity.cs
@@ -21,47 +21,118 @@ namespace cards
 
 			var players = new List<Player>();
 
-			names.ToList().ForEach((name) => {
+			names.ToList().ForEach((name) =>
+			{
 				players.Add(Helper.CreatePlayer(name));
 			});
 
+			var index = 1;
 
-			players.ForEach((p) => { 
+			players.ForEach((p) =>
+			{
 				var row = new TableRow(this);
+				row.Id = index;
 				var playerName = new TextView(this);
 				playerName.Text = p.Name;
 				row.AddView(playerName);
 
 				var maal = new EditText(this);
+				maal.Id = index;
 				row.AddView(maal);
 
 				var gameWon = new CheckBox(this);
+				gameWon.Id = index;
 				gameWon.Checked = p.GameWon;
 				row.AddView(gameWon);
 
 				var maalSeen = new CheckBox(this);
+				maalSeen.Id = index;
 				maalSeen.Checked = p.MaalSeen;
 				row.AddView(maalSeen);
 
 
 				var dubliee = new CheckBox(this);
+				dubliee.Id = index;
 				dubliee.Checked = p.Dubliee;
 				row.AddView(dubliee);
 
 
 				table.AddView(row);
+				index += 1;
 			});
 
 			// logic for calculating maal
 			var calculateButton = FindViewById<Button>(Resource.Id.calculate);
 
-			calculateButton.Click += (sender, e) => {
-				players.ForEach((player) => {
-					player.Points = 20;
-					player.Money = 10;
-				});
-			};
+			calculateButton.Click += (sender, e) =>
+			{
+				var resultPlayers = new List<Player>();
+				players.ForEach((player) =>
+				{
+					var row = (TableRow)FindViewById(players.IndexOf(player) + 1);
+					var name = ((TextView)row.GetChildAt(0)).Text;
+					var maal = ((TextView)row.GetChildAt(1)).Text;
+					var gameWon = ((CheckBox)row.GetChildAt(2)).Checked;
+					var maalSeen = ((CheckBox)row.GetChildAt(3)).Checked;
+					var dubliee = ((CheckBox)row.GetChildAt(4)).Checked;
 
+					int result = 0;
+					var newPlayer = new Player
+					{
+						Name = name,
+						Mall = int.TryParse(maal, out result) ? result : 0,
+						GameWon = gameWon,
+						MaalSeen = maalSeen,
+						Dubliee = dubliee,
+						Points = 10,
+						Money = 20
+					};
+					resultPlayers.Add(newPlayer);
+				});
+
+				var resultTable = FindViewById<TableLayout>(Resource.Id.resultTable);
+				resultTable.RemoveAllViews();
+
+				var titlerow = new TableRow(this);
+				var playerNameText = new TextView(this);
+				playerNameText.SetPadding(0, 0, 100, 0);
+				playerNameText.Text = "Name";
+				titlerow.AddView(playerNameText);
+
+			
+				var pointsText = new TextView(this);
+				pointsText.Text = "Points";
+				pointsText.SetPadding(0, 0, 100, 0);
+				titlerow.AddView(pointsText);
+
+			
+				var moneyText = new TextView(this);
+				moneyText.Text = "Money";
+				moneyText.SetPadding(0, 0, 100, 0);
+				titlerow.AddView(moneyText);
+
+				resultTable.AddView(titlerow);
+
+				resultPlayers.ForEach((p) =>
+					{
+						var row = new TableRow(this);
+
+						var playerName = new TextView(this);
+						playerName.Text = p.Name;
+						row.AddView(playerName);
+
+						var points = new TextView(this);
+						points.Text = p.Points.ToString();
+						row.AddView(points);
+
+						var money = new TextView(this);
+						money.Text = p.Money.ToString();
+						row.AddView(money);
+
+						resultTable.AddView(row);
+						index += 1;
+					});
+			};
 		}
 	}
 
@@ -72,6 +143,10 @@ namespace cards
 			Name = name;
 		}
 
+		public Player()
+		{
+
+		}
 		public string Name { get; set; }
 		public int Mall { get; set; }
 		public bool GameWon { get; set; }
@@ -84,7 +159,8 @@ namespace cards
 
 	public static class Helper
 	{
-		public static Player CreatePlayer(string name) {
+		public static Player CreatePlayer(string name)
+		{
 			return new Player(name);
 		}
 	}
--- 21cards/Resources/Resource.designer.cs
+++ 21cards/Resources/Resource.designer.cs
@@ -44,23 +44,29 @@ namespace cards
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050003
-			public const int addPlayer = 2131034115;
+			// aapt resource value: 0x7f050005
+			public const int addPlayer = 2131034117;
 			
 			// aapt resource value: 0x7f050001
 			public const int calculate = 2131034113;
 			
-			// aapt resource value: 0x7f050002
-			public const int playerName = 2131034114;
-			
 			// aapt resource value: 0x7f050004
-			public const int result = 2131034116;
+			public const int playerName = 2131034116;
 			
-			// aapt resource value: 0x7f050005
-			public const int startGame = 2131034117;
+			// aapt resource value: 0x7f050006
+			public const int result = 2131034118;
+			
+			// aapt resource value: 0x7f050003
+			public const int resultTable = 2131034115;
+			
+			// aapt resource value: 0x7f050007
+			public const int startGame = 2131034119;
 			
 			// aapt resource value: 0x7f050000
 			public const int tableLayout1 = 2131034112;
+			
+			// aapt resource value: 0x7f050002
+			public const int totalMall = 2131034114;
 			
 			static Id()
 			{
